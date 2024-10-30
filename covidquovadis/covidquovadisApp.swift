ae20a0c6c4752ff4ace8ce73681b645b0e488805
Karim Shaloh <kshaloh@gmail.com>
2024-10-30 17:30:33 -0400

    Initial Commit

---
+++ covidquovadis/covidquovadisApp.swift
@@ -0,0 +1,17 @@
+//
+//  covidquovadisApp.swift
+//  covidquovadis
+//
+//  Created by Karim Shaloh on 2024-10-30.
+//
+
+import SwiftUI
+
+@main
+struct covidquovadisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
